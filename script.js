2ff577cd2c4451780e4818e7c53612e7951e99ea
Anurag Bachheti <157354714+Anurag-Bachheti@users.noreply.github.com>
2025-08-31 11:46:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-//your JS code here. If required.
+
+
+async function getData() {
+	
+	const people = document.getElementById("people");
+	
+  try {
+    const response = await fetch("employees.json"); 
+    const data = await response.json();  // parse as JSON
+    people.textContent = console.log(data);
+  } catch (error) {
+    console.error("Error fetching data:", error);
+  }
+}
+
+getData();
