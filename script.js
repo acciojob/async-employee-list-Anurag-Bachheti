d0eefa5710c7853bceafb64bb80393c7f8531ce5
Anurag Bachheti <157354714+Anurag-Bachheti@users.noreply.github.com>
2025-09-19 12:40:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,6 +2,6 @@ fetch("employee.json")
 	.then(response => response.json())
 	.then(employees => {
 		const sorted = employee.sort((a,b)=> a.age-b.age);
-		console.log(sorted);
+		console.log(...sorted);
 	})
 	.catch(err => console.error(err));
